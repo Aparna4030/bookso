ec0164ddc8d14e120586ebe99fd0a86ef05bd73f
Your Name <aparnanambiampurath245@gmail.com>
2024-05-30 13:46:42 +0530

    removed deprecated packages

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,10 @@ const { MONGO_URI } = process.env;
 const connectToDatabase = async () => {
     try {
       await mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
       });
+      console.log('mongo db connected')
     } catch (error) {
       process.exit(1); 
     }
